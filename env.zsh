4ea2ece0769a31f7476943f607962100d2474b94
devk0n <27301010+devk0n@users.noreply.github.com>
2025-09-06 13:42:43 +0200

    Code clean up for better portabilaty

--- env.zsh
+++ env.zsh
@@ -7,6 +7,3 @@ export EDITOR=nvim
 
 # Syntax highlighting
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-export PATH="$HOME/.espressif/tools/xtensa-esp-elf-gdb/16.2_20250324/xtensa-esp-elf-gdb/bin:$PATH"
-export PATH="$HOME/.local/esp-clang/bin:$PATH"
--- plugins.zsh
+++ plugins.zsh
@@ -6,15 +6,3 @@
 ### 1. Enable Zsh completion system ###
 autoload -Uz compinit
 compinit
-
-### 2. Load Zinit if available ###
-if [[ -f "$HOME/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
-  source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-  autoload -Uz _zinit
-  (( ${+_comps} )) && _comps[zinit]=_zinit
-
-  # Plugins
-  zinit light zsh-users/zsh-syntax-highlighting
-else
-  echo "⚠️ Zinit is not installed properly."
-fi
